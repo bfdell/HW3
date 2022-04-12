40742e7497736d00b394d3590959005eb4176a9a
juananaco <ja14acos@siena.edu>
2022-04-12 15:48:10 -0400

    commit

--- Lab8/make_questions_table.php
+++ Lab8/make_questions_table.php
@@ -1,11 +1,14 @@
 <?php
+
 if ($_GET['key'] != "505") {
     die("Access denied");
 }
 
 require_once("functions.php");
+
 $mysqli = db_connect();
-$sql = 'CREATE TABLE Questions22344 (
+
+$sql = "CREATE TABLE Questions22344 (
     id INT NOT NULLAUTO_INCREMENT,
     question VARCHAR(1024) NOT NULL,
     choice1 VARCHAR(1024) NOT NULL,
@@ -13,11 +16,12 @@ $sql = 'CREATE TABLE Questions22344 (
     choice3 VARCHAR(1024) NOT NULL,
     choice4 VARCHAR(1024) NOT NULL,
     answer INT NOT NULL,
-    PRIMARY KEY (id),
-)';
-$result = $mysqli->query($sql);
+    PRIMARY KEY (id)
+)";
+
+$mysqli->query($sql);
 // Use the result
-$result->close();
+
 $mysqli->close();
 ?>
 
