232f68cb3f6ae16f5f706da93754e96c143a1642
bfdell <bf08dell@siena.edu>
2022-04-17 19:20:15 -0400

    made show_games file

---
+++ Lab9/show_games.php
@@ -0,0 +1,38 @@
+<?php
+require_once("functions.php");
+$mysqli = db_connect();
+
+$result = $mysqli->query("SHOW COLUMNS FROM Games50505");
+echo
+'<table>';
+echo
+'<tr>';
+// echo var_dump($result);
+while ($row = $result->fetch_row()) {
+    echo '<th>' . $row[0] . '</th>';
+}
+echo '</tr>';
+$result->close();
+$result = $mysqli->query("SELECT * FROM Games50505");
+
+while ($row = $result->fetch_row()) {
+    echo '<tr>';
+    foreach ($row as $value) {
+        echo '<td>' . $value . '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+$result->close();
+$mysqli->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<title>Show Games</title>
+
+<body>
+</body>
+
+</html>
