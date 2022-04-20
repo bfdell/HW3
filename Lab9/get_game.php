36ac98cd10feb58f1c9f382664ca8c8d864ed9f9
bfdell <bf08dell@siena.edu>
2022-04-19 22:11:24 -0400

    questions now showing up

--- Lab9/get_game.php
+++ Lab9/get_game.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width= device-width, initial-scale=1,
+shrink-to-fit=no">
+    <title>Get Game</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+</head>
 <?php
 require_once("functions.php");
 $mysqli = db_connect();
@@ -13,54 +23,44 @@ $c4 = $row[4];
 $numQuestions = 5;
 $questionsQuery = "SELECT DISTINCT * FROM Questions50505 ORDER BY RAND() LIMIT $numQuestions";
 $questions = $mysqli->query($questionsQuery);
-for($i = 0; $i < $numQuestions; $i++) {
+for ($i = 0; $i < $numQuestions; $i++) {
 
-    while($question = $questions->fetch_assoc()) {
-        echo "<form method=\"post\" action=\"get_qame.php\">";
+    while ($question = $questions->fetch_assoc()) {
+        echo "<form method=\"post\" action=\"get_game.php\">";
 
         echo "<label>{$question['question']}<br>
-        </label>";
+        </label><br>
+        <div class=\"questionfield\">
+            <label>{$question['choice1']}<br>
+            <input type=\"radio\" name=\"answer\" value=\"1\">
+            </label>
+        </div>
+        <div class=\"questionfield\">
+            <label>{$question['choice2']}<br>
+            <input type=\"radio\" name=\"answer\" value=\"2\">
+            </label>
+        </div>
+        <div class=\"questionfield\">
+            <label>{$question['choice3']}<br>
+            <input type=\"radio\" name=\"answer\" value=\"3\">
+            </label>
+        </div>
+        <div class=\"questionfield\">
+            <label>{$question['choice4']}<br>
+            <input type=\"radio\" name=\"answer\" value=\"4\">
+            </label>
+        </div>
+        <input type=\"submit\" name=\"action\" value=\"Next\">";
 
-        echo "</form>";
+        echo "</form><br>";
     }
 }
-// $result = $mysqli->query("SHOW COLUMNS FROM Questions50505");
-// echo
-// '<table>';
-//     echo
-//     '<tr>';
-//         // echo var_dump($result);
-//         while ($row = $result->fetch_row()) {
-//             echo '<th>' . $row[0] . '</th>';
-//         }
-//         echo '</tr>';
-//         $result->close();
-//         $result = $mysqli->query("SELECT * FROM Questions50505");
-        
-//         while ($row = $result->fetch_row()) {
-//             echo '<tr>';
-//                 foreach ($row as $value) {
-//                     echo '<td>' . $value . '</td>';
-//                 }
-//                 echo '</tr>';
-//             }
-//             echo '</table>';
-//             $result->close();
-            $mysqli->close();
+$questions->close();
+$mysqli->close();
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width= device-width, initial-scale=1,
-shrink-to-fit=no">
-    <title>Get Game</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-</head>
-
-<body>
+<!-- <body>
     <form method="post" action="get_question.php">
         <label><?php echo $q ?><br>
         </label>
@@ -92,6 +92,6 @@ shrink-to-fit=no">
     </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js ">
     </script>
-</body>
+</body> -->
 
 </html>
