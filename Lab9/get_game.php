177e375089919111ea641a86f99cb9db882592e8
bfdell <bf08dell@siena.edu>
2022-04-19 22:38:55 -0400

    bug and organization fixes

--- Lab9/get_game.php
+++ Lab9/get_game.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width= device-width, initial-scale=1,
-shrink-to-fit=no">
+    <meta name="viewport" content="width= device-width, initial-scale=1, shrink-to-fit=no">
     <title>Get Game</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
 </head>
@@ -59,39 +58,11 @@ $questions->close();
 $mysqli->close();
 
 ?>
-
-<!-- <body>
-    <form method="post" action="get_question.php">
-        <label><?php echo $q ?><br>
-        </label>
-        <br>
-        <div class="questionfield">
-            <label><?php echo $c1 ?><br>
-            <input type="radio" name="answer" value="1">
-            </label>
-        </div>
-        <div class="questionfield">
-            <label><?php echo $c2 ?><br>
-            <input type="radio" name="answer" value="2">
-            </label>
-        </div>
-        <div class="questionfield">
-            <label><?php echo $c3 ?><br>
-            <input type="radio" name="answer" value="3">
-            </label>
-        </div>
-        <div class="questionfield">
-            <label><?php echo $c4 ?><br>
-            <input type="radio" name="answer" value="4">
-            </label>
-        </div>
-        <input type="submit" name="action" value="Next">
-    </form>
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js ">
-    </script>
-</body> -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js ">
+</script>
+</body>
 
 </html>
