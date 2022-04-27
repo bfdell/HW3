b87c5771e0725461aa4f6d04458e7039a6585e82
bfdell <bf08dell@siena.edu>
2022-04-27 09:49:44 -0400

    process game only processes on post

--- Lab10/process_game.php
+++ Lab10/process_game.php
@@ -21,43 +21,44 @@ if (!isset($_SESSION['username'])) {
     <?php
     require_once("functions.php");
     $mysqli = db_connect();
+    if (isset($_POST)) {
+        $numwon = 0;
+        $numlost = 0;
+        $questions = $_SESSION['questions'];
+        for ($i = 0; $i < count($questions); $i++) {
+            //Gets question and user's answer
+            $question  = $questions["question$i"];
+            $userChoice = $_POST["answer$i"];
 
-    $numwon = 0;
-    $numlost = 0;
-    $questions = $_SESSION['questions'];
-    for ($i = 0; $i < count($questions); $i++) {
-        //Gets question and user's answer
-        $question  = $questions["question$i"];
-        $userChoice = $_POST["answer$i"];
+            //Gets correct answer to current question
+            $answerQuery = "SELECT answer FROM Questions50505 WHERE question = '$question'";
+            $answer = $mysqli->query($answerQuery)->fetch_array()[0];
 
-        //Gets correct answer to current question
-        $answerQuery = "SELECT answer FROM Questions50505 WHERE question = '$question'";
-        $answer = $mysqli->query($answerQuery)->fetch_array()[0];
-
-        //Validates question
-        echo "<strong>Question " . $i + 1 . ":</strong> Correct answer: $answer &nbsp Your answer: $userChoice ";
-        if ($answer == $userChoice) {
-            echo "<strong>CORRECT</strong>";
-            $numwon++;
-        } else {
-            echo " <strong>INCORRECT</strong>";
-            $numlost++;
+            //Validates question
+            echo "<strong>Question " . $i + 1 . ":</strong> Correct answer: $answer &nbsp Your answer: $userChoice ";
+            if ($answer == $userChoice) {
+                echo "<strong>CORRECT</strong>";
+                $numwon++;
+            } else {
+                echo " <strong>INCORRECT</strong>";
+                $numlost++;
+            }
+            echo "<br>Correct: " . $numwon . " Incorrect: " . $numlost . " <br>";
         }
-        echo "<br>Correct: " . $numwon . " Incorrect: " . $numlost . " <br>";
+        //Print insert message
+        $user = $_SESSION['username'];
+        echo "<br>GAME INSERTED INTO TABLE FOR USER: $user";
+
+        //Increments amount of games played for user in users table
+        $incrementPlayedQuery = "UPDATE Users50505 SET gamesplayed = gamesplayed + 1 WHERE username = '$user'";
+        $mysqli->query($incrementPlayedQuery);
+
+        //Inserts game into table with numwon and lost
+        $insertGameQuery = "INSERT INTO Games50505 (username, numwon, numlost) VALUES ('$user', '$numwon', '$numlost')";
+        $mysqli->query($insertGameQuery);
+
+        $mysqli->close();
     }
-    //Print insert message
-    $user = $_SESSION['username'];
-    echo "<br>GAME INSERTED INTO TABLE FOR USER: $user";
-
-    //Increments amount of games played for user in users table
-    $incrementPlayedQuery = "UPDATE Users50505 SET gamesplayed = gamesplayed + 1 WHERE username = '$user'";
-    $mysqli->query($incrementPlayedQuery);
-
-    //Inserts game into table with numwon and lost
-    $insertGameQuery = "INSERT INTO Games50505 (username, numwon, numlost) VALUES ('$user', '$numwon', '$numlost')";
-    $mysqli->query($insertGameQuery);
-
-    $mysqli->close();
     ?>
     <br>
     <a href="get_game.php">Play Again</a>
