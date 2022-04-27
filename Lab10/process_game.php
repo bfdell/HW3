c2b38c727c957f60e0f7413eb421ba73f6dc5853
bfdell <bf08dell@siena.edu>
2022-04-27 09:46:28 -0400

    added authentication to process game

--- Lab10/process_game.php
+++ Lab10/process_game.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
+require_once("functions.php");
 if (!isset($_SESSION['username'])) {
-    die("You must be logged in to process a game");
+    die("You must be logged in to play a game");
+} else {
+    authenticate();
 }
 ?>
 <!DOCTYPE html>
